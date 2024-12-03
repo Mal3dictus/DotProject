4b71b0cf9be7418f8f5aa2054db40254290d7558
Mal3dictus <bsm302461@gmail.com>
2024-12-04 07:33:49 +0800

    unfinished

---
+++ 02.DogsApp-CRUD-Filter/DogsApp/DogApp.Buisness/Contracts/IDogService.cs
@@ -0,0 +1,23 @@
+﻿using DogsApp.Infrastructure.Data.Domain;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsApp.Buisness.Contracts
+{
+    public interface IDogService
+    {
+        bool Create(string name, int age, string breed, string picture);
+        bool UpdateDog(int dogId, string name, int age, string breed, string picture);
+        bool DeleteDog(int dogId);
+        List<Dog> GetDogs();
+        Dog GetDogById(int dogId);
+        List<Dog> GetDogs(string searchStringBreed, string searchStringName);
+
+
+
+    }
+}
---
+++ 02.DogsApp-CRUD-Filter/DogsApp/DogApp.Buisness/Services/DogService.cs
@@ -0,0 +1,34 @@
+﻿using DogsApp.Buisness.Contracts;
+using DogsApp.Infrastructure.Data;
+using DogsApp.Infrastructure.Data.Domain;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogsApp.Buisness.Services
+{
+    public class DogService: IDogService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DogService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public bool Create(string name, int age, string breed, string? picture)
+        {
+            Dog item = new Dog
+            {
+                Name = name,
+                Age = age,
+                Breed = breed,
+                Picture = picture,
+            };
+            _context.Dogs.Add(item);
+            return _context.SaveChanges() !=0;
+        }
+    }
+}
